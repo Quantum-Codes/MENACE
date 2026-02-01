632e7b5d560969219999d6abf84e783083535f41
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2026-02-01 07:01:50 +0000

    player win check

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Menace - Matchbox Educable Noughts and Crosses Engine
 One of the first Reinforcement Learning algorithms made. This was used to play tic-tac-toe intelligently.
 
 ## Behaviour
-MENACE doesn't know the rules of Tic-Tac-Toe. It doesn't know that three-in-a-row wins. It only picks 
+MENACE doesn't know the rules of Tic-Tac-Toe. It doesn't know that three-in-a-row wins. It only picks out of a learned whitelist moves from each game board possibility.
+We need to teach it this whitelist (The ideal moves on every game board possible)
--- main.py
+++ main.py
@@ -4,18 +4,19 @@ def permutation(possibilities):
 
     
 def game_state():
-    solutions = [""]
+    all_states = [""]
     for i in range(9):
-        solutions.extend(permutation(solutions))
+        all_states.extend(permutation(all_states))
     
-    for string in solutions:
-        grid = [["" for i in range(3)] for j in range(3)]
+    for string in all_states:
+        grid = [[" " for i in range(3)] for j in range(3)]
         for i in range(9):
-            grid[i//3][i%3] = 1
+            grid[i // 3][i % 3] = string[i]
 
 
 
 def print_board(board):
+    print()
     print(" | ".join(board[0]))
     print("-" * 9)
     print(" | ".join(board[1]))
@@ -34,10 +35,29 @@ def player_turn(board, player):
         except (ValueError, IndexError):
             print("Invalid input. Please enter numbers between 0 and 2.")
 
+def check_winner(board):
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != " ":
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != " ":
+            return board[0][i]
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return board[0][2]
+    return None
 
 def game_loop(board, player):
     print_board(board)
     player_turn(board, player)
+    winner = check_winner(board)
+
+    if winner:
+        print_board(board)
+        print(f"Player {winner} wins!")
+        exit()
     
 
 
