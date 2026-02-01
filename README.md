5b53861c96c7ad5cbbd3e85bcf53a78a6603c775
Quantum-Codes <87054411+Quantum-Codes@users.noreply.github.com>
2026-02-01 08:04:11 +0000

    save before moving to string
    Co-authored-by: Anmol Sinha <anmolsinhaiit@gmail.com>

--- README.md
+++ README.md
@@ -7,3 +7,76 @@ One of the first Reinforcement Learning algorithms made. This was used to play t
 ## Behaviour
 MENACE doesn't know the rules of Tic-Tac-Toe. It doesn't know that three-in-a-row wins. It only picks out of a learned whitelist moves from each game board possibility.
 We need to teach it this whitelist (The ideal moves on every game board possible)
+
+This is done by adding beads to a matchbox as it was done originally, and then pick a random bead corresponding to the move it needs to play (so there will be 9 coloured beads)
+Each matchbox in the system represents a unique state of the Tic-Tac-Toe board.
+The Memory: Inside each box are beads of different colours. Each colour corresponds to a potential move (an empty square) on the grid. There can be multiple beads of same colour indicating this move has been responsible for a lot of wins and the random picking will most likely pick that colour again.
+
+On a Win: MENACE is rewarded. Each matchbox used in that game receives 3 additional beads of the colour that was played. This makes it much more likely to repeat those winning moves in the future.
+
+On a Draw: It receives a small reward of 1 additional bead.
+
+On a Loss: It is punished. The beads used in that game are removed from their boxes. This decreases the chance of the machine making that losing mistake again.
+
+There is a possibility that a box empties itself in the process - this means that the game state always leads to a loss and there is no way to recover OR it was in early game and the bot had too much of a skill issue to win (and so we might either have to restart training or make sure each early game state always has 1 of each bead, we will decide which to choose later)
+
+Also typically we always let the bot start so it only learns X's moves or Y's moves (these are disjoint to one another). I would want them to play against each other and improve each other, and due to this disjoint property I can just use the same set of matchboxes and in the end have a singular model that can play both X and O.
+
+<br>
+
+# Creating the tictactoe game itself
+We need to create a standard tictactoe game first and then put this system on it.
+
+## 1. Storing the game state
+So first we will decide how to store the game - 2D lists seem like a natural choice because of the 3x3 board. We can also use a length 9 string (and that will be better as we figure out later) but we will stick to 2D lists since this is a beginner blog
+
+If you are using strings then this is how you index: "012345678" and that corresponds to: <br>
+
+<table>
+  <thead>
+    <tr>
+      <th></th>
+      <th><b>col0</b></th>
+      <th><b>col1</b></th>
+      <th><b>col2</b></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><b>row0</b></td>
+      <td>0</td>
+      <td>1</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td><b>row1</b></td>
+      <td>3</td>
+      <td>4</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td><b>row2</b></td>
+      <td>6</td>
+      <td>7</td>
+      <td>8</td>
+    </tr>
+  </tbody>
+</table>
+
+You can see that <br>
+`index = row * 3 + col` <br>
+`col = index % 3`<br>
+`row = index // 3`
+
+Also we will fill the table with X, O and " " (a space for empty cell)
+
+Let us first create the board (2d list)
+```python
+board = [[" ", " ", " "] for _ in range(3)]
+# This is list comprehension. This is the shorter version of:
+board = []
+for _ in range(3):
+    board.append([" ", " ", " "])
+
+# the string guys would just do board = " "*9
+```
--- main.py
+++ main.py
@@ -1,17 +1,63 @@
 def permutation(possibilities):
+    states = []
     for possibility in possibilities:
-        return [possibility+" ",possibility+"X",possibility+"O"]
-
+        states.extend([possibility+" ",possibility+"X",possibility+"O"])
+    return states 
     
 def game_state():
+    # all game states in string format
+    
+    # Recurrence relation: (T = string of gamestates, n = number of moves made)
+    # Base case: T(0) = ""
+    # T(n) = T(n-1) + " "
+    #      = T(n-1) + "X"
+    #      = T(n-1) + "O"
+    # We end at n = 9. So loop with 9 iterations
+    # this will give us all possible combinations of X, O, and empty spaces in a 3x3 grid = (3 possibilities)^(9 positions) = 19683 states
     all_states = [""]
     for i in range(9):
-        all_states.extend(permutation(all_states))
+        all_states = permutation(all_states)
     
-    for string in all_states:
+    #remove all where (number of X) - (number of O) > 1 since we can only have alternating moves and X always starts
+    for state in all_states:
+        if (state.count("X") - state.count("O")) > 1:
+            all_states.remove(state)
+
+    #remove rotations and mirrored states. 
+    # For this we pick a state one at a time, generate all its rotations and mirrored versions, and then check if any of those already exist in new_states. IF not we add them.
+    unique_states = []
+    index_map = [6,3,0,7,4,1,8,5,2] # 90 deg rotated indexes. we can repeat this rotation again and again for every rotation.
+    mirror_y_map = [2,1,0,5,4,3,8,7,6] # mirrored indexes along y axis
+    mirror_x_map = [6,7,8,3,4,5,0,1,2] # mirrored indexes along x axis
+    for state in all_states:
+        
+        to_rotate = list(state)
+        y_mirror = [state[mirror_y_map[i]] for i in range(9)]
+        x_mirror = [state[mirror_x_map[i]] for i in range(9)]
+        similar_states = [state,x_mirror,y_mirror]  #similar states contins all states equivalent to the selected state
+        for i in range(3):
+            rotated_state = [to_rotate[index_map[i]] for i in range(9)]
+            to_rotate = list(rotated_state)
+            similar_states.append(rotated_state)
+
+        for item in similar_states:      
+            if item in unique_states:
+                break
+        else:
+            unique_states.append(state) #if none matches then for loop is not broken and the state is added
+
+    #all game states in grid format
+    sample_space = []
+    for state in unique_states:
         grid = [[" " for i in range(3)] for j in range(3)]
         for i in range(9):
-            grid[i // 3][i % 3] = string[i]
+            grid[i // 3][i % 3] = state[i]
+        sample_space.append(grid)
+
+    
+    return sample_space
+
+    
 
 
 
@@ -29,7 +75,7 @@ def player_turn(board, player):
             num = int(input(f"Player {player}, enter the cell number (0-8): "))
             if board[num // 3][num % 3] == " ":
                 board[num // 3][num % 3] = player
-                break
+                return num
             else:
                 print("That position is already taken. Try again.")
         except (ValueError, IndexError):
@@ -42,6 +88,7 @@ def check_winner(board):
             return board[i][0]
         if board[0][i] == board[1][i] == board[2][i] != " ":
             return board[0][i]
+        
     # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] != " ":
         return board[0][0]
@@ -51,20 +98,28 @@ def check_winner(board):
 
 def game_loop(board, player):
     print_board(board)
-    player_turn(board, player)
-    winner = check_winner(board)
-
-    if winner:
-        print_board(board)
-        print(f"Player {winner} wins!")
-        exit()
+    move = player_turn(board, player)
+    return move
     
 
 
 if __name__ == "__main__":
     board = [[" ", " ", " "] for _ in range(3)]
-
+    X_move_list = []
+    O_move_list = []
     player = 1 # 1 = X, 0 = Y
     while True:
-        game_loop(board, "X" if player == 1 else "O")
+        move = game_loop(board, "X" if player == 1 else "O")
+        if player == 1:
+            X_move_list.append(move)
+        else:
+            O_move_list.append(move)
+        
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            print(f"Player {winner} wins!")
+            break
+        
+        
         player = 1 - player # Switch player
