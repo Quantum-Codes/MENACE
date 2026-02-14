5fca413c113025f722336acb4b8e88641454cb5f
anmol <anmolsinhaiit@gmail.com>
2026-02-14 21:22:08 +0530

    fixed an issue where 2 consecutive played states had different orientations in unique states

--- menace.py
+++ menace.py
@@ -14,14 +14,16 @@ class AI:
     
     def reset_game_state(self, winner):
         
-        played_states = generate_played_states(self.X_move_list, self.O_move_list, self.unique_states)
+        
         if winner == "X" and self.player == 1 or winner == "O" and self.player == 0:
             win = 1
         elif winner == "X" and self.player == 0 or winner == "O" and self.player == 1:
             win = -1
         else:
             win = 0
-        self.unique_states = update_beads(played_states,self.unique_states,win)
+        played_states, played_indexes = generate_played_states(self.X_move_list, self.O_move_list, self.unique_states)[0], generate_played_states(self.X_move_list, self.O_move_list, self.unique_states)[1]
+        self.unique_states = update_beads(played_states,played_indexes,self.unique_states,win)
+
 
 
         self.X_move_list.clear()
@@ -123,23 +125,33 @@ def filter_game_states():
     return unique_states
 
 def generate_played_states(X_moves, O_moves, unique_states):#generate all board positions that happend
-    played_states = []
+    played_states = ["         "] #the first state
+    prev_order = "012345678"
     state = [" " for j in range(9)]
+    idx = []
     x_idx = o_idx = 0
-    while x_idx<len(X_moves): #X and O are put in same iteration. So, x_idx is checked as it starts first
+    while x_idx<=len(X_moves): #X and O are put in same iteration. So, x_idx is checked as it starts first
         if x_idx==o_idx:
             state[X_moves[x_idx]] = "X"
+            added_idx = X_moves[x_idx]
             x_idx+=1
+            
         else:
             state[O_moves[o_idx]] = "O"
+            added_idx = O_moves[o_idx]
             o_idx+=1
-        winner = check_winner("".join(state))
-        if winner==None and state.count(" ")>1: 
-            played_states.append(find_similar_states("".join(state),unique_states))
-        else:
-            pass #The winning states are not stored in the unique states.
             
-    return played_states
+        winner = check_winner("".join(state))
+        if winner==None and state.count(" ")>1:
+            idx.append(prev_order.index(str(added_idx)))
+            present_state, prev_order = find_similar_states("".join(state),unique_states)[0], find_similar_states("".join(state),unique_states)[1] 
+            played_states.append(present_state)
+            
+        else:
+            idx.append(prev_order.index(str(added_idx)))
+            break #The winning states are not stored in the unique states.
+            
+    return (played_states,idx)
 
 
 def find_similar_states(state, unique_states):
@@ -162,22 +174,37 @@ def find_similar_states(state, unique_states):
         similar_states.append(to_rotate)
         similar_states.append(to_rotate_y)
         similar_states.append(to_rotate_x)
-
+    idx = ["0123456789",
+            "678345012",
+            "210543876",
+            "630741852",
+            "852741630",
+            "036147258",
+            "876543210",
+            "678345012",
+            "210543876",
+            "258147036",
+            "036147258",
+            "852741630"]
+    
+    count = 0
     for item in similar_states:  #it is guaranteed that one of the similar states exist as all possible unique board is stored
         if item in unique_states:
-            return item
+            return (item,idx[count])
+        count+=1
 
     
 
    
-def update_beads(played_states,unique_states,win): #win=1 if the ai wins, win=0 if it draws and win=-1 for a loss
+def update_beads(played_states,played_indexes,unique_states,win): #win=1 if the ai wins, win=0 if it draws and win=-1 for a loss
     #played states is the list of states played in the position
-    for state in played_states:
-        beads = unique_states[state]
-        for i in range(9):
-            if beads[i]!=0:
-                beads[i] = ((win//2)+1)*(beads[i] + win*2+1)  # if win=1 => +3; win=0 => +1; win=-1 => remove all
-        unique_states[state] = beads
+    
+
+    for i in range(len(played_states)):
+        beads = unique_states[played_states[i]]
+        if beads[played_indexes[i]]!=0:
+            beads[played_indexes[i]] = ((win//2)+1)*(beads[played_indexes[i]] + win*2+1)  # if win=1 => +3; win=0 => +1; win=-1 => remove all
+        unique_states[played_states[i]] = beads
     return unique_states
                 
 
